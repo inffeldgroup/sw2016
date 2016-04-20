22b7783643d45735295e3f5e2f6d517c86de345f
Markus Schofnegger <mschofnegger@student.tugraz.at>
2016-04-20 12:09:19 +0200

    [MS,MST] ImageFetcher implemented

--- app/src/main/java/at/tugraz/inffeldgroup/dailypic/ImageFetcher.java
+++ app/src/main/java/at/tugraz/inffeldgroup/dailypic/ImageFetcher.java
@@ -1,9 +1,34 @@
 package at.tugraz.inffeldgroup.dailypic;
 
+import android.app.*;
+import android.content.Context;
+import android.database.Cursor;
+import android.net.*;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+
+import java.util.*;
+import java.io.*;
+
 /**
  * Created by markus on 13/04/16.
  */
 public class ImageFetcher {
+    private Activity activity;
+    private int view_size;
+    private int history_size;
+    private ArrayList<Integer> seeds;
+    private int current_seed_index;
+    public void init(Activity activity){
+        this.activity = activity;
+        this.view_size = 6;
+        this.history_size = 3;
+
+        this.current_seed_index = -1;
+        this.seeds = new ArrayList<Integer>();
+    }
+
     private static ImageFetcher ourInstance = new ImageFetcher();
 
     public static ImageFetcher getInstance() {
@@ -12,4 +37,74 @@ public class ImageFetcher {
 
     private ImageFetcher() {
     }
+
+    public ArrayList<Uri> getPreviousRandomImagePaths() {
+        if(this.seeds.size() <= 0) {
+            return null;
+        }
+        if(this.current_seed_index <= 0) {
+            return null;
+        }
+        if(this.current_seed_index <= (this.seeds.size() - this.history_size)) {
+            return null;
+        }
+
+        this.current_seed_index--;
+        int seed = this.seeds.get(this.current_seed_index);
+        ArrayList<Uri> image_paths = getRandomImagePaths(seed);
+        return image_paths;
+    }
+
+    public ArrayList<Uri> getNextRandomImagePaths() {
+
+        int seed;
+        this.current_seed_index++;
+        if(this.current_seed_index > this.seeds.size() - 1) {
+            Random rand_gen = new Random();
+            seed = rand_gen.nextInt();
+            this.seeds.add(seed);
+        }
+        else {
+            seed = this.seeds.get(this.current_seed_index);
+        }
+
+        ArrayList<Uri> image_paths = getRandomImagePaths(seed);
+        return image_paths;
+    }
+
+    public ArrayList<Uri> getRandomImagePaths(int seed) {
+
+        // Get picture directory
+        Uri images = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+        String[] projection = new String[] { MediaStore.Images.Media.DATA };
+        Cursor cursor = this.activity.getContentResolver().query(images, projection, "", null, "");
+
+        ArrayList<String> paths_all = new ArrayList<String>();
+        ArrayList<Uri> paths_specific = new ArrayList<Uri>();
+
+        if(cursor.moveToFirst()) {
+            int data_column = cursor.getColumnIndex(MediaStore.Images.Media.DATA);
+
+            String path_string;
+            do {
+                path_string = cursor.getString(data_column);
+                paths_all.add(path_string);
+            } while(cursor.moveToNext());
+
+            Random rand_gen = new Random(seed);
+            int image_index;
+            Uri path_uri;
+            for(int i = 0; i < this.view_size; i++) {
+                image_index = Math.abs(rand_gen.nextInt()) % paths_all.size();
+                path_uri = Uri.fromFile(new File(paths_all.get(image_index)));
+                paths_specific.add(path_uri);
+            }
+
+        }
+
+        cursor.close();
+
+        return paths_specific;
+    }
+
 }
--- app/src/main/java/at/tugraz/inffeldgroup/dailypic/MainActivity.java
+++ app/src/main/java/at/tugraz/inffeldgroup/dailypic/MainActivity.java
@@ -2,6 +2,7 @@ package at.tugraz.inffeldgroup.dailypic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d("[DEBUG]", "App starting.");
+
+        ImageFetcher.getInstance().init(MainActivity.this);
+
+        Log.d("[DEBUG]", "PREVIOUS:");
+        ImageFetcher.getInstance().getPreviousRandomImagePaths();
+        Log.d("[DEBUG]", "NEXT:");
+        ImageFetcher.getInstance().getNextRandomImagePaths();
+        Log.d("[DEBUG]", "NEXT:");
+        ImageFetcher.getInstance().getNextRandomImagePaths();
+        Log.d("[DEBUG]", "NEXT:");
+        ImageFetcher.getInstance().getNextRandomImagePaths();
+        Log.d("[DEBUG]", "PREVIOUS:");
+        ImageFetcher.getInstance().getPreviousRandomImagePaths();
+        Log.d("[DEBUG]", "NEXT:");
+        ImageFetcher.getInstance().getNextRandomImagePaths();
+
     }
 }
