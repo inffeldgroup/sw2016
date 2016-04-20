4ffd9ce5a43eff5fc0c3ef2d56b59d219e2a343f
Markus Schrotter <iknuppe@gmx.at>
2016-04-20 10:18:22 +0200

    [MaS MxS] Sharebutton + Fullscreen style

--- app/src/main/java/at/tugraz/inffeldgroup/dailypic/MainActivity.java
+++ app/src/main/java/at/tugraz/inffeldgroup/dailypic/MainActivity.java
@@ -1,16 +1,24 @@
 package at.tugraz.inffeldgroup.dailypic;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportActionBar().hide();
-
 
         setContentView(R.layout.activity_main);
+    }
+    public void sharebuttonOnClick(View v)
+    {
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_TEXT, "This is my text to send.");
+        sendIntent.setType("text/plain");
+        startActivity(sendIntent);
     }
 }
