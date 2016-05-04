d41bc0559f65d43ae2565dbbb1fa0ae104bb73f8
Marco Starke <m.starke@student.tugraz.at>
2016-05-04 16:15:30 +0200

    [FK,MST] double click to add favourites

--- app/src/main/java/at/tugraz/inffeldgroup/dailypic/FavouriteHandler.java
+++ app/src/main/java/at/tugraz/inffeldgroup/dailypic/FavouriteHandler.java
@@ -3,6 +3,7 @@ package at.tugraz.inffeldgroup.dailypic;
 import android.content.Context;
 import android.net.*;
 import android.os.Environment;
+import android.provider.BaseColumns;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -14,15 +15,15 @@ import java.util.*;
  */
 public class FavouriteHandler {
 
-    private static final String folder_name = "/DailyPicFavs/";
+    public static final String folder_name = "/DailyPicFavs/";
     public static final String fav_folder_path = Environment.getExternalStorageDirectory().getAbsolutePath() + "/" + Environment.DIRECTORY_PICTURES + FavouriteHandler.folder_name;
 
     public FavouriteHandler() {
 
     }
 
-    public boolean moveImgsToFavFolder(Context context, List<Uri> favs) {
-
+    public boolean moveImgsToFavFolder(Context context, Uri uri) {
+        // TODO: distinguish between move to fav folder or to WHICH OTHER direcotry?
         File f = new File(FavouriteHandler.fav_folder_path);
 
         if (f.exists() == false) {
@@ -33,18 +34,18 @@ public class FavouriteHandler {
         }
 
         boolean ret = true;
-        for (Uri uri : favs) {
-            File src = new File(uri.getPath());
-            File dest = new File(FavouriteHandler.fav_folder_path + src.getName());
 
-            if (src.renameTo(dest) == false) {
-                Log.e("[DAYLIPIC]", "Error moving file to favourites folder: " + src.getAbsolutePath());
-                ret = false;
-            } else {
-                Log.d("[DAYLIPIC]", "Moved image to favourites folder: " + src.getName());
-                Toast.makeText(context, "Moved image to DailyPicFavs foler: " + src.getName(), Toast.LENGTH_LONG);
-            }
+        File src = new File(uri.getPath());
+        File dest = new File(FavouriteHandler.fav_folder_path + src.getName());
+
+        if (src.renameTo(dest) == false) {
+            Log.e("[DAYLIPIC]", "Error moving file to favourites folder: " + src.getAbsolutePath());
+            ret = false;
+        } else {
+            Log.d("[DAYLIPIC]", "Moved image to favourites folder: " + src.getName());
+            Toast.makeText(context, "Moved image to DailyPicFavs foler: " + src.getName(), Toast.LENGTH_LONG);
         }
+
         return ret;
     }
 
--- app/src/main/java/at/tugraz/inffeldgroup/dailypic/MainActivity.java
+++ app/src/main/java/at/tugraz/inffeldgroup/dailypic/MainActivity.java
@@ -2,6 +2,7 @@ package at.tugraz.inffeldgroup.dailypic;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,6 +11,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,7 +19,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
     public static final int numberOfItems = 6;
     private ImageFetcher img_fetcher;
-    private List<Uri> img_list;
+    private ArrayList<Uri> uriList;
     private GridView gridView;
     private ImageGridViewAdapter gridAdapter;
     private FavouriteHandler favhandler;
@@ -27,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         this.img_fetcher = new ImageFetcher(MainActivity.this);
-        final ArrayList<Uri> uriList = img_fetcher.getNextRandomImages(numberOfItems);
+        this.uriList = img_fetcher.getNextRandomImages(numberOfItems);
 
         this.favhandler = new FavouriteHandler();
 
@@ -35,18 +37,70 @@ public class MainActivity extends AppCompatActivity {
         gridView = (GridView) findViewById(R.id.mainGridView);
         gridView.setAdapter(gridAdapter);
 
-        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        abstract class DoubleClickListener implements AdapterView.OnItemClickListener {
+            private static final long DOUBLE_CLICK_TIME_DELTA = 500; //milliseconds
+
+            private CountDownTimer timer;
+            long lastClickTime = 0;
+
+            private void startSingleClickTimer(final View view, final int position) {
+                lastClickTime = System.currentTimeMillis();
+                timer = new CountDownTimer(DOUBLE_CLICK_TIME_DELTA, DOUBLE_CLICK_TIME_DELTA) {
+                    @Override
+                    public void onTick(long l) {}
+
+                    @Override
+                    public void onFinish() {
+                        onSingleClick(view, position);
+                    }
+                };
+                timer.start();
+            }
+
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            public void onItemClick(AdapterView<?> parent, final View view, final int position, long id) {
+
+                if (lastClickTime == 0) {
+                    startSingleClickTimer(view, position);
+                } else {
+                    if (System.currentTimeMillis() - lastClickTime < DOUBLE_CLICK_TIME_DELTA) {
+                        timer.cancel();
+                        lastClickTime = 0;
+                        onDoubleClick(view, position);
+                    } else {
+                        if (timer != null) {
+                            timer.cancel();
+                        }
+                        startSingleClickTimer(view, position);
+                    }
+                }
+            }
+
+            public abstract void onSingleClick(View v, int position);
+            public abstract void onDoubleClick(View v, int position);
+        }
+
+        gridView.setOnItemClickListener(new DoubleClickListener() {
+            @Override
+            public void onDoubleClick(View v, int position) {
+                Uri uri = uriList.get(position);
+                MainActivity.this.favhandler.moveImgsToFavFolder(getApplicationContext(), uri);
+                Toast.makeText(getApplicationContext(), "Added to favourites: " + uri.getLastPathSegment().toString(), Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onSingleClick(View v, int position) {
                 Intent intent = new Intent(MainActivity.this, FullscreenImage.class);
                 intent.setData(uriList.get(position));
                 startActivity(intent);
             }
         });
+
         gridView.setOnTouchListener(new View.OnTouchListener(){
 
             @Override
             public boolean onTouch(View v, MotionEvent event) {
+
                 if(event.getAction() == MotionEvent.ACTION_MOVE){
                     return true;
                 }
@@ -66,40 +120,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void backButtonOnClick(View v) {
-        final ArrayList<Uri> uriList = this.img_fetcher.getPrevRandomImages(numberOfItems);
+        uriList = this.img_fetcher.getPrevRandomImages(numberOfItems);
         gridAdapter.setNewImages(uriList);
         gridAdapter.notifyDataSetChanged();
-        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = new Intent(MainActivity.this, FullscreenImage.class);
-                intent.setData(uriList.get(position));
-                startActivity(intent);
-            }
-        });
     }
 
-    public void favButtonOnClick(View v){
-        // TODO: remove this and add doubleclick listener!
-        this.favhandler.moveImgsToFavFolder(this, this.img_fetcher.getNextRandomImages(1));
-
+    public void favButtonOnClick(View v) {
         Intent intent = new Intent(MainActivity.this, FavouriteActivity.class);
         startActivity(intent);
     }
 
     public void nextButtonOnClick(View v) {
-        final ArrayList<Uri> uriList = this.img_fetcher.getNextRandomImages(numberOfItems);
+        uriList = this.img_fetcher.getNextRandomImages(numberOfItems);
 
         gridAdapter.setNewImages(uriList);
         gridAdapter.notifyDataSetChanged();
-        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = new Intent(MainActivity.this, FullscreenImage.class);
-                intent.setData(uriList.get(position));
-                startActivity(intent);
-            }
-        });
     }
 }
 
