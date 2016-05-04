0378108400dac23281a40adaf60e6c1640e31930
Christian Russ <cruss@student.tugraz.at>
2016-05-04 13:56:34 +0200

    [CR MaS] added share button functionality and selection

--- app/src/main/java/at/tugraz/inffeldgroup/dailypic/MainActivity.java
+++ app/src/main/java/at/tugraz/inffeldgroup/dailypic/MainActivity.java
@@ -5,8 +5,13 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.SparseBooleanArray;
+import android.view.ActionMode;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.ImageView;
@@ -27,11 +32,81 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         this.img_fetcher = new ImageFetcher(MainActivity.this);
         final ArrayList<Uri> uriList = img_fetcher.getNextRandomImages(numberOfItems);
+        img_list = uriList;
 
         gridAdapter = new ImageGridViewAdapter(this, uriList);
         gridView = (GridView) findViewById(R.id.mainGridView);
         gridView.setAdapter(gridAdapter);
 
+
+        gridView.setMultiChoiceModeListener(new AbsListView.MultiChoiceModeListener() {
+            @Override
+            public void onItemCheckedStateChanged(ActionMode mode, int position, long id, boolean checked) {
+
+                Log.d("xD", Integer.toString(position) + " " + Boolean.toString(gridView.isItemChecked(position)));
+                if(!gridView.isItemChecked(position)){
+                    gridView.setItemChecked(position, true);
+
+                    gridView.setAlpha(0.5f);
+                }
+                else {
+                    gridView.setAlpha(1);
+                    gridView.setItemChecked(position, false);
+                }
+            }
+
+            @Override
+            public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+                gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        if(!gridView.isItemChecked(position)){
+                            gridView.setItemChecked(position, true);
+
+                            view.setAlpha(0.5f);
+                        }
+                        else {
+                            view.setAlpha(1);
+                            gridView.setItemChecked(position, false);
+                        }
+                    }
+                });
+
+                return false;
+            }
+
+            @Override
+            public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+                return false;
+            }
+
+            @Override
+            public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+                if(!item.isChecked()){
+                    item.setChecked(true);
+                    item.getActionView().setAlpha(0.5f);
+                }
+                else {
+                    item.getActionView().setAlpha(1);
+                    item.setChecked(false);
+                }
+                return false;
+            }
+
+            @Override
+            public void onDestroyActionMode(ActionMode mode) {
+                gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        Intent intent = new Intent(MainActivity.this, FullscreenImage.class);
+                        intent.setData(uriList.get(position));
+                        startActivity(intent);
+                    }
+                });
+                gridView.setChoiceMode(AbsListView.CHOICE_MODE_NONE);
+
+            }
+        });
         gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -40,6 +115,27 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        gridView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                gridView.setChoiceMode(AbsListView.CHOICE_MODE_MULTIPLE_MODAL);
+                Log.d("xD", Integer.toString(position) + " " + Boolean.toString(gridView.isItemChecked(position)));
+                if(!gridView.isItemChecked(position)){
+                    gridView.setItemChecked(position, true);
+
+                    view.setAlpha(0.5f);
+                }
+                else {
+                    view.setAlpha(1);
+                    gridView.setItemChecked(position, false);
+                }
+                return true;
+            }
+        });
+
+
         gridView.setOnTouchListener(new View.OnTouchListener(){
 
             @Override
@@ -56,14 +152,25 @@ public class MainActivity extends AppCompatActivity {
     public void sharebuttonOnClick(View v)
     {
         Intent sendIntent = new Intent();
-        sendIntent.setAction(Intent.ACTION_SEND);
-        sendIntent.putExtra(Intent.EXTRA_TEXT, "This is my text to send.");
-        sendIntent.setType("text/plain");
-        startActivity(sendIntent);
+        sendIntent.setAction(Intent.ACTION_SEND_MULTIPLE);
+        sendIntent.setType("image/*");
+        if(gridView.getCheckedItemPositions() != null) {
+            SparseBooleanArray checked = gridView.getCheckedItemPositions();
+            ArrayList<Uri> shareList = new ArrayList<Uri>();
+            for (int i = 0; i < gridView.getCount(); i++)
+                if (checked.get(i)) {
+                    Log.d("asdf", "test" + checked.toString());
+                    shareList.add(img_list.get(i));
+                }
+            sendIntent.putParcelableArrayListExtra(Intent.EXTRA_STREAM, shareList);
+            startActivity(sendIntent);
+        }
     }
 
     public void backButtonOnClick(View v) {
         final ArrayList<Uri> uriList = this.img_fetcher.getPrevRandomImages(numberOfItems);
+        img_list = uriList;
+        clearSelection();
         gridAdapter.setNewImages(uriList);
         gridAdapter.notifyDataSetChanged();
         gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -83,7 +190,8 @@ public class MainActivity extends AppCompatActivity {
 
     public void nextButtonOnClick(View v) {
         final ArrayList<Uri> uriList = this.img_fetcher.getNextRandomImages(numberOfItems);
-
+        img_list = uriList;
+        clearSelection();
         gridAdapter.setNewImages(uriList);
         gridAdapter.notifyDataSetChanged();
         gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -95,6 +203,32 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    public void onBackPressed()
+    {
+        if(gridView.getChoiceMode() != AbsListView.CHOICE_MODE_MULTIPLE_MODAL)
+          super.onBackPressed();
+        clearSelection();
+    }
+
+    public void clearSelection(){
+        if(gridView.getChoiceMode() == AbsListView.CHOICE_MODE_MULTIPLE_MODAL) {
+            for (int i = 0; i < gridView.getCount(); i++) {
+                gridView.getChildAt(i).setAlpha(1);
+                gridView.setItemChecked(i, false);
+            }
+            gridView.setChoiceMode(AbsListView.CHOICE_MODE_NONE);
+            gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    Intent intent = new Intent(MainActivity.this, FullscreenImage.class);
+                    intent.setData(img_list.get(position));
+                    startActivity(intent);
+                }
+            });
+        }    }
 }
+
 
 
