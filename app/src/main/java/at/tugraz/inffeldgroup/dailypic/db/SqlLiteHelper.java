53c48eb593ad439a5dfac8416e2f5584107a4634
Felix Kirchengast <felix.kirchengast@gmail.com>
2016-05-11 10:23:14 +0200

    [FK,MST] Created database structure for sqlite

--- app/src/main/java/at/tugraz/inffeldgroup/dailypic/db/SqlLiteHelper.java
+++ app/src/main/java/at/tugraz/inffeldgroup/dailypic/db/SqlLiteHelper.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 /**
  * Helper class for SQL operations implemented as Singleton design pattern.
@@ -12,94 +13,27 @@ import android.database.sqlite.SQLiteOpenHelper;
  */
 public class SqlLiteHelper extends SQLiteOpenHelper
 {
+	private static final String TAG = SqlLiteHelper.class.getSimpleName();
 
 	/** Instance of the database */
 	private static volatile SqlLiteHelper	instance										= null;
+
 
 	private static final int					DATABASE_VERSION							= 1;
 	public static final String					DATABASE_NAME								= "MainDatabase.db";
 
 	public static final String					TABLE_FAVORITES									= "Favorites";
+	public static final String					COLUMN_FAVORITE_ID					= "favID";
 	public static final String					COLUMN_URI								= "URI";
 
-	public static final String					TABLE_PATIENTS								= "Patients";
-	public static final String					COLUMN_PATIENT_ID							= "pId";
-	public static final String					COLUMN_PATIENT_FIRSTNAME				= "pFirstName";
-	public static final String					COLUMN_PATIENT_LASTNAME					= "pLastName";
-	public static final String					COLUMN_PATIENT_PATIENT_NUMBER			= "pPatientNumber";
-
-	public static final String					TABLE_MEASUREMENTS						= "Measurements";
-	public static final String					COLUMN_MEASUREMENT_ID					= "mId";
-	public static final String					COLUMN_MEASUREMENT_PATIENT_ID			= "mPId";
-	public static final String					COLUMN_MEASUREMENT_ASIG_ID				= "mAId";
-	public static final String					COLUMN_MEASUREMENT_START_TIME			= "mStartTime";
-	public static final String					COLUMN_MEASUREMENT_STOP_TIME			= "mStopTime";
-	public static final String					COLUMN_MEASUREMENT_INTERVAL			= "mInterval";
-
-	public static final String					TABLE_TEMPERATURES						= "Temperatures";
-	public static final String					COLUMN_TEMPERATURE_ID					= "tId";
-	public static final String					COLUMN_TEMPERATURE_MEASUREMENT_ID	= "tMId";
-	public static final String					COLUMN_TEMPERATURE_TEMPERATURE		= "tTemp";
-	public static final String					COLUMN_TEMPERATURE_TIME					= "tTime";
-
-	public static final String					TABLE_CREATE_PATIENTS					= "CREATE TABLE "
-																												+ TABLE_PATIENTS
+	public static final String					TABLE_CREATE_FAVORITES					= "CREATE TABLE "
+																												+ TABLE_FAVORITES
 																												+ "("
-																												+ COLUMN_PATIENT_ID
+																												+ COLUMN_FAVORITE_ID
 																												+ " INTEGER PRIMARY KEY AUTOINCREMENT, "
-																												+ COLUMN_PATIENT_FIRSTNAME
-																												+ " TEXT NOT NULL, "
-																												+ COLUMN_PATIENT_LASTNAME
-																												+ " TEXT NOT NULL, "
-																												+ COLUMN_PATIENT_PATIENT_NUMBER
-																												+ " TEXT);";
+																												+ COLUMN_URI
+																												+ " TEXT NOT NULL);";
 
-	public static final String					TABLE_CREATE_ASIGS						= "CREATE TABLE "
-																												+ TABLE_ASIGS
-																												+ "("
-																												+ COLUMN_ASIG_ID
-																												+ " INTEGER PRIMARY KEY AUTOINCREMENT, "
-																												+ COLUMN_ASIG_UID
-																												+ " TEXT NOT NULL, "
-																												+ COLUMN_ASIG_WAIT_COEFF
-																												+ " INTEGER DEFAULT 2000000, "
-																												+ COLUMN_ASIG_TEMPERATURE_COEFF_D
-																												+ " DOUBLE DEFAULT 330, "
-																												+ COLUMN_ASIG_TEMPERATURE_COEFF_K
-																												+ " DOUBLE DEFAULT -0.3221, "
-																												+ COLUMN_ASIG_TYPE
-																												+ " INTEGER NOT NULL);";
-
-	public static final String					TABLE_CREATE_MEASUREMENTS				= "CREATE TABLE "
-																												+ TABLE_MEASUREMENTS
-																												+ "("
-																												+ COLUMN_MEASUREMENT_ID
-																												+ " INTEGER PRIMARY KEY AUTOINCREMENT, "
-																												+ COLUMN_MEASUREMENT_PATIENT_ID
-																												+ " INTEGER NOT NULL REFERENCES "
-																												+ TABLE_PATIENTS + "(" + COLUMN_PATIENT_ID + ")" + " ON DELETE CASCADE, "
-																												+ COLUMN_MEASUREMENT_ASIG_ID
-																												+ " INTEGER NOT NULL REFERENCES "
-																												+ TABLE_ASIGS + "(" + COLUMN_ASIG_ID + ")" + " ON DELETE CASCADE, "
-																												+ COLUMN_MEASUREMENT_START_TIME
-																												+ " INTEGER NOT NULL, "
-																												+ COLUMN_MEASUREMENT_STOP_TIME
-																												+ " INTEGER NOT NULL, "
-																												+ COLUMN_MEASUREMENT_INTERVAL
-																												+ " INTEGER NOT NULL);";
-
-	public static final String					TABLE_CREATE_TEMPERATURES				= "CREATE TABLE "
-																												+ TABLE_TEMPERATURES
-																												+ "("
-																												+ COLUMN_TEMPERATURE_ID
-																												+ " INTEGER PRIMARY KEY AUTOINCREMENT, "
-																												+ COLUMN_TEMPERATURE_MEASUREMENT_ID
-																												+ " INTEGER NOT NULL REFERENCES "
-																												+ TABLE_MEASUREMENTS + "(" + COLUMN_MEASUREMENT_ID + ")" + " ON DELETE CASCADE, "
-																												+ COLUMN_TEMPERATURE_TEMPERATURE
-																												+ " DOUBLE NOT NULL, "
-																												+ COLUMN_TEMPERATURE_TIME
-																												+ " INTEGER NOT NULL);";
 
 	/**
 	 * Creates or returns the the SQL database
@@ -145,14 +79,11 @@ public class SqlLiteHelper extends SQLiteOpenHelper
 	{
 		try
 		{
-			database.execSQL(TABLE_CREATE_ASIGS);
-			database.execSQL(TABLE_CREATE_PATIENTS);
-			database.execSQL(TABLE_CREATE_MEASUREMENTS);
-			database.execSQL(TABLE_CREATE_TEMPERATURES);
+			database.execSQL(TABLE_CREATE_FAVORITES);
 		}
 		catch (SQLException e)
 		{
-			dbErrorHandler.addError(e);
+			Log.e(TAG, "Creation of database failed.");
 		}
 	}
 	
@@ -185,26 +116,12 @@ public class SqlLiteHelper extends SQLiteOpenHelper
 	{
 		try
 		{
-			db.execSQL("DROP TABLE IF EXISTS " + TABLE_ASIGS);
-			db.execSQL("DROP TABLE IF EXISTS " + TABLE_PATIENTS);
-			db.execSQL("DROP TABLE IF EXISTS " + TABLE_MEASUREMENTS);
-			db.execSQL("DROP TABLE IF EXISTS " + TABLE_TEMPERATURES);
+			db.execSQL("DROP TABLE IF EXISTS " + TABLE_FAVORITES);
 		}
 		catch (SQLException e)
 		{
-			dbErrorHandler.addError(e);
+			Log.e(TAG, "Upgrade to new database version failed.");
 		}
 		onCreate(db);
-	}
-
-	/**
-	 * Gets called to create the table structure.
-	 * 
-	 * @return the actual DBErrorHandler
-	 * 
-	 */
-	public DBErrorHandler getDBErrorHandler()
-	{
-		return dbErrorHandler;
 	}
 }
