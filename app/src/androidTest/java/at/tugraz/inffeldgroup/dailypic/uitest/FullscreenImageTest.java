54ba9ea7e792d149144ebf7d6411656c0176d745
rebex1 <rene.a.berger@gmail.com>
2016-05-18 16:18:32 +0000

    [RB MST] Testcase for picture details

--- app/src/androidTest/java/at/tugraz/inffeldgroup/dailypic/uitest/FullscreenImageTest.java
+++ app/src/androidTest/java/at/tugraz/inffeldgroup/dailypic/uitest/FullscreenImageTest.java
@@ -1,12 +1,15 @@
 package at.tugraz.inffeldgroup.dailypic.uitest;
 
+import android.media.Image;
 import android.test.ActivityInstrumentationTestCase2;
+import android.view.View;
 
 import com.robotium.solo.Solo;
 
 import junit.framework.TestCase;
 
 import at.tugraz.inffeldgroup.dailypic.FullscreenImage;
+import at.tugraz.inffeldgroup.dailypic.ImageItem;
 
 /**
  * Created by christian on 20.04.16.
@@ -23,12 +26,16 @@ public class FullscreenImageTest extends ActivityInstrumentationTestCase2 {
         mySolo = new Solo(getInstrumentation(),getActivity());
     }
 
-    public void tearDown() throws Exception {
-
-    }
 
     public void testCloseButton() {
         mySolo.clickOnButton("Close");
     }
 
+
+    public void tearDown() throws Exception{
+        mySolo.clickLongOnScreen(10,10);
+        mySolo.waitForDialogToOpen();
+        mySolo.waitForDialogToClose();
+
+    }
 }
