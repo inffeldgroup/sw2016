4be8d29ff82af9286031b86ee76785be2ac06b4b
Christian Russ <cruss@student.tugraz.at>
2016-06-08 13:32:23 +0200

    [MST CR] added tests for preinstalling images

--- app/src/androidTest/java/at/tugraz/inffeldgroup/dailypic/test/test_MainActivity.java
+++ app/src/androidTest/java/at/tugraz/inffeldgroup/dailypic/test/test_MainActivity.java
@@ -1,19 +1,90 @@
 package at.tugraz.inffeldgroup.dailypic.test;
 
+import android.app.Activity;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.test.ActivityInstrumentationTestCase2;
+import android.util.Log;
+
 import junit.framework.Assert;
 
 import at.tugraz.inffeldgroup.dailypic.activities.MainActivity;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import at.tugraz.inffeldgroup.dailypic.ImageFetcher;
 import at.tugraz.inffeldgroup.dailypic.db.UriWrapper;
 
 public class test_MainActivity extends ActivityInstrumentationTestCase2<MainActivity> {
-
+    private static boolean firstRun = true;
     public test_MainActivity() {
         super(MainActivity.class);
+    }
+
+    public static ArrayList<String> getImagesPath(Activity activity) {
+        Uri uri;
+        ArrayList<String> listOfAllImages = new ArrayList<String>();
+        Cursor cursor;
+        int column_index_data, column_index_folder_name;
+        String PathOfImage = null;
+        uri = android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+
+        String[] projection = { MediaStore.MediaColumns.DATA,
+                MediaStore.Images.Media.BUCKET_DISPLAY_NAME };
+
+        cursor = activity.getContentResolver().query(uri, projection, null,
+                null, null);
+
+        column_index_data = cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA);
+        column_index_folder_name = cursor
+                .getColumnIndexOrThrow(MediaStore.Images.Media.BUCKET_DISPLAY_NAME);
+        while (cursor.moveToNext()) {
+            PathOfImage = cursor.getString(column_index_data);
+
+            listOfAllImages.add(PathOfImage.substring(PathOfImage.lastIndexOf("/")+1, PathOfImage.length()));
+        }
+        return listOfAllImages;
+    }
+
+    protected void setUp() {
+        if (firstRun) {
+            firstRun = false;
+
+            File dir = new File(Environment.getExternalStorageDirectory() + "/Daily Pic");
+            if (dir.isDirectory())
+            {
+                String[] children = dir.list();
+                for (int i = 0; i < children.length; i++)
+                {
+                    new File(dir, children[i]).delete();
+                }
+            }
+            dir.delete();
+        }
+    }
+
+    public void testPreinstallImages(){
+        String[] assets = null;
+        try {
+            assets = getActivity().getResources().getAssets().list("pictures");
+            File dir = new File(Environment.getExternalStorageDirectory() + "/Daily Pic");
+            ArrayList<String> file_list = new ArrayList<String>(Arrays.asList(dir.list(null)));
+            for (String name : assets) {
+                assertTrue(file_list.contains(name));
+            }
+            ArrayList<String> media_list = getImagesPath(this.getActivity());
+            for (String name : assets) {
+                assertTrue(media_list.contains(name));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
     public void testDoNotShowSamePicturesInOneGrid() {
